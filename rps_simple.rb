b61975b73bb8b9bac6003c8fecf35efcd9913beb
Ashley M. Jones <ashleyj38@gmail.com>
2016-01-26 19:09:57 -0500

    pseudocode

---
+++ rps_simple.rb
@@ -0,0 +1,38 @@
+#Instructions
+#Create a game that plays until the user wants to stop
+#Have a player select an item (rock, paper, or scissors)
+#Have the computer select a random item
+#Use branching to determine who wins
+
+
+#Rules
+#Rock beats scissors
+#Paper beats rock
+#Scissors beats paper
+
+
+####################################################################
+
+
+#Create variables
+
+
+#start loop
+#Ask the user for their choice
+
+
+#Initialize the computer's choice
+
+
+#Determine winner
+
+
+
+#Print resulting winner
+
+
+
+#Ask the user to play again
+
+
+#end loop
